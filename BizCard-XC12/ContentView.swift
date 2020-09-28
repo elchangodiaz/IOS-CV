de56dde104c0d3f9b16e8c1ffee3ebad1e7e63c4
elchangodiaz <elchangodiaz1@gmail.com>
2020-09-28 16:22:51 -0700

    adding project

--- BizCard-XC12/ContentView.swift
+++ BizCard-XC12/ContentView.swift
@@ -1,19 +1,42 @@
-//
-//  ContentView.swift
-//  BizCard-XC12
-//
-//  Created by javier mercado on 28/09/20.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack{
+            
+            Image("JMV")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle()
+                            .stroke(Color.white, lineWidth: 3))
+                .shadow(radius: 6)
+            
+            VStack(){
+                Text("Javier Mercado V.")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                Text("Developer")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                HStack{
+                    Image(systemName: "g.square.fill")
+                        .foregroundColor(.black)
+                    Text("https://github.com/elchangodiaz")
+                        .font(.subheadline)
+                        .foregroundColor(Color.white)
+                        .bold()
+                        .italic()
+                }
+            }
+        }.frame(width: 300, height: 200)
+        .background(Color.blue)
+        .cornerRadius(8)
+        .shadow(radius: 5)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
